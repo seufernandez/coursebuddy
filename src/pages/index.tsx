9750ada1aa8c3135ec2e6e695823d167a6024adc
seufernandez <seufernandezdev@gmail.com>
2021-10-09 01:02:47 -0300

    🆙update: non logged main page translated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { GetServerSideProps, GetStaticProps } from 'next';
 import { getSession, useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
@@ -29,17 +30,18 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <aside>
           <h1>
-            Se junte a <span>milhares de estudantes</span> por todo o Brasil
+            Join with <span>thousands of students</span> over the World
           </h1>
           <p>
-            Resumos sobre os melhores cursos do mercado, networking e premiações
+            You'll find: Resumes, Jobs, Articles, Classes, Tests and more than
+            you can Imagine!
           </p>
-          <strong>Compartilhe conhecimento e cresça junto com a gente!</strong>
+          <strong>Share your knowledge and grow with us!</strong>
           <Box align="right" width="100%" mt="4">
             <Link href="/community">
               <a>
                 <Button colorScheme="green" borderRadius="2xl" size="lg">
-                  Entrar na Comunidade
+                  Join Community!
                 </Button>
               </a>
             </Link>
