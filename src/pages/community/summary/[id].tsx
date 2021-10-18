a1f1315ca1db07da53866ac05d55843bbe0273f8
seufernandez <seufernandezdev@gmail.com>
2021-10-18 18:33:37 -0300

    update: Summary Page restyled

--- src/pages/community/summary/[id].tsx
+++ src/pages/community/summary/[id].tsx
@@ -8,6 +8,7 @@ import {
   Spacer,
   Button,
   Img,
+  Icon,
   Link as ChakraLink,
   useToast,
 } from '@chakra-ui/react';
@@ -17,6 +18,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { getSession, useSession } from 'next-auth/client';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { FiExternalLink } from 'react-icons/fi';
 
 import { supabase } from '../../../services/supabase';
 import { NotFound } from '../../../components/NotFound';
@@ -122,173 +124,192 @@ export default function Resume({
         <title>{resume.name}</title>
       </Head>
 
-      <Box
-        display={{ md: 'flex' }}
-        width="90%"
-        maxWidth={1080}
-        mx="auto"
-        mt={8}
-        alignSelf="center"
-        border="1px"
-        borderColor="purple.500"
-        borderRadius="2xl"
-        justifyContent="center"
+      <Flex
+        w="100%"
+        h="calc(100vh - 6rem)"
+        py={{ base: '0', md: '20' }}
+        align="center"
       >
         <Box
-          position="relative"
-          maxHeight={{ sm: '240px', md: 'lg', lg: 'lg' }}
-          h={{ lg: 'lg', md: 'lg', sm: '240px' }}
-          w={{ sm: '100%', md: '50%', lg: '50%' }}
+          display={{ md: 'flex' }}
+          width="90%"
+          maxWidth={1080}
+          mx="auto"
+          alignSelf="center"
+          border="1px"
+          borderColor="purple.500"
           borderRadius="2xl"
+          justifyContent="center"
         >
-          <Img
-            maxHeight={{ lg: 'lg', md: 'lg', sm: '240px' }}
+          <Box
+            position="relative"
+            maxHeight={{ sm: '240px', md: 'lg', lg: 'lg' }}
             h={{ lg: 'lg', md: 'lg', sm: '240px' }}
-            w="50%"
-            boxSize="100%"
-            objectFit="cover"
-            src={
-              resume.image !== ''
-                ? resume.image
-                : '/assets/illustrations/ResumeImageSource.svg'
-            }
-            fallback="/assets/illustrations/ResumeImageSource.svg"
-            alt="Course Image"
+            w={{ sm: '100%', md: '50%', lg: '50%' }}
             borderRadius="2xl"
-          />
-
-          <VStack
-            position="absolute"
-            bottom="20px"
-            px="8"
-            py="4"
-            bg="purple.900"
-            alignItems="left"
-            borderRightRadius="16"
-            spacing="1/4"
           >
-            <Text fontWeight="400" fontStyle="Italic" color="purple.500">
-              By:
-            </Text>
-            <Text fontWeight="600" fontSize="xl">
-              {resume.users.name}
-            </Text>
-          </VStack>
-        </Box>
-
-        <VStack
-          bg="purple.900"
-          h={{ md: 'lg', sm: 'md' }}
-          w={{ sm: '100%', md: '50%', lg: '50%' }}
-          minWidth={{ md: '50%', lg: '50%' }}
-          alignItems="left"
-          borderBottomRadius="2xl"
-          borderTopRightRadius="2xl"
-        >
-          <Text
-            mb={['1', '2', '4']}
-            fontWeight="700"
-            fontSize={['xl', '2xl', '3xl']}
-            lineHeight="9"
-            px={['6', '8']}
-            pt={['2', '3', '6']}
-          >
-            {resume.name}
-          </Text>
-
-          <Text
-            px={['6', '8']}
-            lineHeight="5"
-            color="purple.500"
-            noOfLines={6}
-            fontSize={['sm', 'md', 'lg']}
-          >
-            {resume.description}
-          </Text>
-
-          <Spacer />
-
-          <Box px="8">
-            <Text fontWeight="400" fontStyle="Italic" color="purple.500" mb="1">
-              Link:
-            </Text>
-            <Box
-              px="4"
-              py="2"
+            <Img
+              maxHeight={{ lg: 'lg', md: 'lg', sm: '240px' }}
+              h={{ lg: 'lg', md: 'lg', sm: '240px' }}
+              w="50%"
+              boxSize="100%"
+              objectFit="cover"
+              src={
+                resume.image !== ''
+                  ? resume.image
+                  : '/assets/illustrations/ResumeImageSource.svg'
+              }
+              fallback="/assets/illustrations/ResumeImageSource.svg"
+              alt="Course Image"
               borderRadius="2xl"
-              border="1px"
-              borderColor="purple.500"
+            />
+
+            <VStack
+              position="absolute"
+              bottom="20px"
+              px={['4', '4', '8']}
+              py={['2', '4', '4']}
+              bg="purple.900"
+              alignItems="left"
+              borderRightRadius="16"
+              spacing="1/4"
             >
               <Text
-                fontWeight="200"
-                fontSize={['lg', 'xl']}
-                color="gray.700"
-                noOfLines={2}
-                as="abbr"
-              >
-                {resume.link}
-              </Text>
-            </Box>
-
-            <Flex align="center" justify="end" mt="4" mb="4" w="100%">
-              <Link href={`/community/course/${resume.courses.id}`}>
-                <Button
-                  colorScheme="purple"
-                  color="purple.500"
-                  h="10"
-                  w="40"
-                  mr="4"
-                  variant="outline"
-                  _hover={{ bg: 'purple.700' }}
-                >
-                  Course Page
-                </Button>
-              </Link>
-
-              <ChakraLink href={resume.link} isExternal h="10">
-                <Button
-                  colorScheme="purple"
-                  color="purple.900"
-                  w="56"
-                  _focus={{ boxShadow: 'none' }}
-                  _hover={{ bg: 'purple.550', textDecoration: 'none' }}
-                >
-                  Go to Link
-                </Button>
-              </ChakraLink>
-            </Flex>
-          </Box>
-          <Box align="right" w="100%">
-            <HStack
-              w="16"
-              bgColor="purple.800"
-              borderBottomRightRadius="2xl"
-              borderTopLeftRadius="2xl"
-            >
-              <Button
-                pl="5"
-                size="lg"
+                fontSize={['sm', 'sm', 'md']}
+                fontWeight="400"
+                fontStyle="Italic"
                 color="purple.500"
-                variant="unstyled"
-                fontSize="2xl"
-                justifyContent="center"
-                onClick={handleLikeResume}
-                isDisabled={notUser}
               >
-                {liked ? <AiFillHeart /> : <AiOutlineHeart />}
-              </Button>
-            </HStack>
+                By:
+              </Text>
+              <Text fontWeight="600" fontSize={['md', 'lg', 'xl']}>
+                {resume.users.name}
+              </Text>
+            </VStack>
           </Box>
-        </VStack>
-      </Box>
 
-      <Box
+          <VStack
+            bg="purple.900"
+            h={{ md: 'lg', sm: 'md' }}
+            w={{ sm: '100%', md: '50%', lg: '50%' }}
+            minWidth={{ md: '50%', lg: '50%' }}
+            alignItems="left"
+            borderBottomRadius="2xl"
+            borderTopRightRadius="2xl"
+          >
+            <Text
+              mb={['1', '2', '4']}
+              fontWeight="700"
+              fontSize={['xl', '2xl', '3xl']}
+              lineHeight="9"
+              px={['6', '8']}
+              pt={['2', '3', '6']}
+            >
+              {resume.name}
+            </Text>
+
+            <Text
+              px={['6', '8']}
+              lineHeight="5"
+              color="purple.500"
+              noOfLines={6}
+              fontSize={['sm', 'md', 'lg']}
+            >
+              {resume.description}
+            </Text>
+
+            <Spacer />
+
+            <Box px="8">
+              <Text
+                fontWeight="400"
+                fontStyle="Italic"
+                color="purple.500"
+                mb="1"
+              >
+                Link:
+              </Text>
+              <Box
+                px="4"
+                py="2"
+                borderRadius="2xl"
+                border="1px"
+                borderColor="purple.500"
+              >
+                <Text
+                  fontWeight="200"
+                  fontSize={['lg', 'xl']}
+                  color="gray.700"
+                  noOfLines={2}
+                  as="abbr"
+                >
+                  {resume.link}
+                </Text>
+              </Box>
+
+              <Flex align="center" justify="end" mt="4" mb="4" w="100%">
+                <Link href={`/community/course/${resume.courses.id}`}>
+                  <Button
+                    size="md"
+                    colorScheme="purple"
+                    color="purple.500"
+                    h="10"
+                    mr="4"
+                    variant="outline"
+                    _hover={{ bg: 'purple.700' }}
+                  >
+                    Course Page
+                  </Button>
+                </Link>
+
+                <ChakraLink href={resume.link} isExternal h="10">
+                  <Button
+                    size="md"
+                    colorScheme="purple"
+                    color="purple.900"
+                    _focus={{ boxShadow: 'none' }}
+                    _hover={{ bg: 'purple.550', textDecoration: 'none' }}
+                  >
+                    Go to Link
+                    <Icon ml="2" mt="2" h="6" w="6" align="center">
+                      <FiExternalLink />
+                    </Icon>
+                  </Button>
+                </ChakraLink>
+              </Flex>
+            </Box>
+            <Box align="right" w="100%">
+              <HStack
+                w="16"
+                bgColor="purple.800"
+                borderBottomRightRadius="2xl"
+                borderTopLeftRadius="2xl"
+              >
+                <Button
+                  pl="5"
+                  size="lg"
+                  color="purple.500"
+                  variant="unstyled"
+                  fontSize="2xl"
+                  justifyContent="center"
+                  onClick={handleLikeResume}
+                  isDisabled={notUser}
+                >
+                  {liked ? <AiFillHeart /> : <AiOutlineHeart />}
+                </Button>
+              </HStack>
+            </Box>
+          </VStack>
+        </Box>
+      </Flex>
+
+      {/* <Box
         width="90%"
         maxWidth={1080}
         mx="auto"
         my={12}
         justifyContent="center"
-      />
+      /> */}
     </>
   );
 }
