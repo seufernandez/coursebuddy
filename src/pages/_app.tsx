fa6b37335134cd83862c5a6b8ce366d7eb80e1f1
seufernandez <seufernandezdev@gmail.com>
2021-10-27 19:10:44 -0300

    feat: source meta descrition added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import { AppProps } from 'next/app';
 // import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
 import { Provider as NextauthProvider } from 'next-auth/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -7,14 +8,21 @@ import Header from '../components/Header';
 
 import '../styles/globals.scss';
 import { theme } from '../styles/theme';
+import useLocale from '../services/hooks/useLocale';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const t = useLocale();
+
   return (
     <QueryClientProvider client={queryClient}>
       <NextauthProvider session={pageProps.session}>
         <ChakraProvider theme={theme}>
+          <Head>
+            <meta name="description" content={t.document.description} />
+          </Head>
+
           <Header />
           <Component {...pageProps} />
         </ChakraProvider>
